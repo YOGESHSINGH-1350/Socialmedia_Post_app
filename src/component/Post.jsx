ada310e95dfda5def596fc2fd5202258bdcc8304
Yogesh Singh <singhyogesh1305@gmail.com>
2024-12-19 21:15:49 +0530

    Update Post.jsx

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -9,9 +9,7 @@ const{deletepost}=useContext(postlistcontext)
 
       <h5 className='card-title'>{post.title}</h5>
 
-      <p className="card-text">
-       {post.body}
-      </p>
+      <p className="card-text"> {post.body} </p>
 
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
         <button className='deletbutton'> 
